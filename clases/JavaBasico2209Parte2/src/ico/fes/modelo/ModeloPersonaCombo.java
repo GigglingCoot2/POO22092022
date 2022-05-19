a596f66807873c8f779b3eee17645045b0e1d8b3
GigglingCoot2 <ximenarodriguez78@aragon.unam.mx>
2022-05-19 12:59:19 -0500

    Clase 19-05-2022

---
+++ clases/JavaBasico2209Parte2/src/ico/fes/modelo/ModeloPersonaCombo.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes.modelo;
+
+import ico.fes.herencia.Persona;
+import java.util.ArrayList;
+import javax.swing.ComboBoxModel;
+import javax.swing.event.ListDataListener;
+
+/**
+ *
+ * @author begax
+ */
+public class ModeloPersonaCombo implements ComboBoxModel<Persona>{
+    private ArrayList<Persona> datos;
+    private Persona selected;
+
+    public ModeloPersonaCombo() {
+    }
+
+    public ModeloPersonaCombo(ArrayList datos, Persona selected) {
+        this.datos = datos;
+        this.selected = selected;
+    }
+
+    @Override
+    public void setSelectedItem(Object o) {
+        this.selected = (Persona) o;
+    }
+
+    @Override
+    public Object getSelectedItem() {
+        return selected;
+    }
+
+    @Override
+    public int getSize() {
+        //regresará el numero de elementos a mostrat
+        return datos.size();
+    }
+
+    @Override
+    public Persona getElementAt(int i) {
+       return datos.get(i);
+    }
+
+    @Override
+    public void addListDataListener(ListDataListener l) {
+        
+    }
+
+    @Override
+    public void removeListDataListener(ListDataListener l) {
+        
+    }
+    
+    public void consultarBaseDatos(){
+        //simular una bd
+        datos = new ArrayList<Persona>();
+        //conextiarn a bd
+        //consulta SQL
+        datos.add(new Persona("José", 19));
+        datos.add(new Persona("Maria", 21));
+        datos.add(new Persona("Jesus", 33));
+        datos.add(new Persona("Diana", 22));
+    }
+    
+    
+}
